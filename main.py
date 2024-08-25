f95dcf10369791dd93f94d829fdb4294c8b29712
bryantruong-99 <blt010@ucsd.edu>
2024-08-25 00:28:29 -0700

    Added google gemini

--- main.py
+++ main.py
@@ -2,12 +2,8 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime
-import wikipedia
 import webbrowser
 import os
-import smtplib
-import requests
-from bs4 import BeautifulSoup
 import pyglet
 import time
 import threading
@@ -15,6 +11,7 @@ from urllib.request import urlopen
 import re
 import sys
 from playsound import playsound
+import google.generativeai as genai
 
 user = "Bryan"
 
@@ -31,144 +28,111 @@ class TTS():
         self.rate = self.engine.getProperty("rate")
         self.engine.setProperty("voice", self.voices[1].id)
         self.engine.setProperty("rate", 200)
+
     # Converts text to speech and reads aloud the text put into the function
     def speak(self, text):
         self.engine.say(text)
         self.engine.runAndWait()
 
-###########################################
-#Greets user based on the time of the day
-def greeting():
-    tts = TTS()
-    hour = int(datetime.datetime.now().hour)
+class Brybot():
 
-    if hour > 0 and hour < 12:
-        tts.speak("Good Morning" + user)
-    elif hour >= 12 and hour < 18:
-        tts.speak("Good Afternoon" + user)
-    else:
-        tts.speak("Good Evening" + user)
+    def __init__(self):
+        self.tts = TTS()
 
-    tts.speak("How may I help you today?")
-    del(tts)
+        genai.configure(api_key=os.environ["API_KEY"])
+        self.gemini_model = genai.GenerativeModel("gemini-1.5-flash")
 
-#If I want to send emails use this function
-def sendEmail(to, content):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
-    server.login("myemail@gmail.com", "password")
-    server.sendmail("email@gmail.com")
-    server.close
+    #Greets user based on the time of the day
+    def greeting(self):
+        hour = int(datetime.datetime.now().hour)
 
-#Searches youtube and opens the first result
-#WIP
-def searchYT(text):
-    search_keyword = text.replace(" ", "+")
-    html = urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    link = "https://www.youtube.com/watch?v=" + video_ids[0]
+        if 0 < hour < 12:
+            self.tts.speak("Good Morning" + user)
+        elif 12 <= hour < 18:
+            self.tts.speak("Good Afternoon" + user)
+        else:
+            self.tts.speak("Good Evening" + user)
 
-    webbrowser.open_new(link)
+        self.tts.speak("How may I help you today?")
 
-def searchGoogle(text):
-    search_keyword = text.replace(" ","+")
-    webbrowser.open("https://www.google.com/search?q=" + search_keyword)
+    #Searches youtube and opens the first result
+    #WIP
+    def search_yt(self, text):
+        search_keyword = text.replace(" ", "+")
+        html = urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+        link = "https://www.youtube.com/watch?v=" + video_ids[0]
 
-#Setting up pyglet window to display and play T-MO gif
-def displayGIF(local_dir):
-    tmo_gif = os.path.join(local_dir,"images\\T-MO.gif")
-    ani = pyglet.image.load_animation(tmo_gif)
-    aniSprite = pyglet.sprite.Sprite(ani)
+        webbrowser.open_new(link)
 
-    w = aniSprite.width
-    h = aniSprite.height
+    #Setting up pyglet window to display and play T-MO gif
+    def display_gif(self, local_dir):
+        tmo_gif = os.path.join(local_dir,"images\\T-MO.gif")
+        ani = pyglet.image.load_animation(tmo_gif)
+        aniSprite = pyglet.sprite.Sprite(ani)
 
-    window = pyglet.window.Window(width=w, height=h)
+        w = aniSprite.width
+        h = aniSprite.height
 
-    r,g,b,alpha = 0.5, 0.5, 0.8, 0.5
+        window = pyglet.window.Window(width=w, height=h)
 
-    pyglet.gl.glClearColor(r,g,b,alpha)
+        r,g,b,alpha = 0.5, 0.5, 0.8, 0.5
 
-    @window.event
-    def on_draw():
-        window.clear()
-        aniSprite.draw()
+        pyglet.gl.glClearColor(r,g,b,alpha)
 
-    pyglet.app.run()
+        @window.event
+        def on_draw():
+            window.clear()
+            aniSprite.draw()
 
-def startBryBot(query):
+        pyglet.app.run()
 
-    text_speech = TTS()
+    def gemini_response(self, query):
+        response = self.gemini_model.generate_content(query)
 
-    #Where the audio portion begins
+        return response.text
 
-    if "wikipedia" in query.lower():
-        # playsound(wario_fine)
-        text_speech.speak("Searching Wikipedia...")
-        query = query.lower().replace("wikipedia","")
-        results = wikipedia.summary(query, sentences = 3)
-        text_speech.speak(results)
+    def start_bry_bot(self, query):
+        lower_query = query.lower()
 
-    elif "youtube" in query.lower():
-        text_speech.speak("On it")
-        query = query.lower().replace("youtube","")
-        searchYT(query)
+        if "youtube" in lower_query:
+            self.tts.speak("On it")
+            query = query.lower().replace("youtube","")
+            self.search_yt(query)
 
-    elif "google" in query.lower():
-        text_speech.speak("I got you, homie")
-        query = query.lower().replace("google","")
-        searchGoogle(query)
+        elif "play music" in lower_query:
+            song_dir= "C:\\Users\\bryan\\Music".encode("utf-8")
+            songs = os.listdir(song_dir)
+            os.startfile(os.path.join(song_dir,songs[1]))
 
-    elif "play music" in query.lower():
-        song_dir= "C:\\Users\\bryan\\Music".encode("utf-8")
-        songs = os.listdir(song_dir)
-        os.startfile(os.path.join(song_dir,songs[1]))
+        elif "epic" in lower_query:
+            self.tts.speak("Hell Yeah!")
+            webbrowser.open("https://www.youtube.com/watch?v=dGJlZw4FYgE")
 
-    elif "epic" in query.lower():
-        text_speech.speak("Hell Yeah!")
-        webbrowser.open("https://www.youtube.com/watch?v=dGJlZw4FYgE")
+        elif "the time" in lower_query:
+            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+            self.tts.speak(f"{user}, the time is {current_time}")
 
-    elif "the time" in query.lower():
-        time = datetime.datetime.now().strftime("%H:%M:%S")
-        text_speech.speak(f"{user}, the time is {time}")
+        elif "play games" in lower_query:
+            os.system("emulationstation")
+            sys.exit(0)
 
-    elif "send email" in query.lower():
-        try:
-            speak("Who should I send it to")
-            to = command()
-            speak("What do you want to send")
-            content = command()
-            sendEmail(to, content)
-            speak("The email has been sent!")
+        elif "thank you" in lower_query:
+            self.tts.speak("You're welcome" + user)
 
-        except Exception as e:
-            print(e)
+        elif "close windows" in query.lower():
+            os.system("taskkill /im chrome.exe /f")
 
-    elif "play games" in query.lower():
-        os.system("emulationstation")
-        sys.exit(0)
+        elif "turn off" in lower_query:
+            self.tts.speak("Goodbye" + user)
+            os.system("shutdown /s /t 1")
 
-    elif "tell me a joke" in query.lower():
-        text_speech.speak("Yo momma")
-
-    elif "song" in query.lower():
-        text_speech.speak("")
-
-    elif "thank you" in query.lower():
-        text_speech.speak("You're welcome" + user)
-    elif "close windows" in query.lower():
-        os.system("taskkill /im chrome.exe /f")
-
-    elif "turn off" in query.lower():
-        text_speech.speak("Goodbye" + user)
-        os.system("shutdown /s /t 1")
-
-    del(text_speech)
+        else:
+            self.tts.speak(self.gemini_response(lower_query))
 
 
 #Continuous speech recognition using sphinx and google api
-def command():
+def command(brybot):
     query = None
     #Adjust threshold for speech so it does not recognize background noise
     r = sr.Recognizer()
@@ -181,14 +145,13 @@ def command():
     source = sr.Microphone()
 
     def callback(recognizer, audio):  # this is called from the background thread
-        print("Hi")
         try:
             speech_as_text = recognizer.recognize_sphinx(audio, keyword_entries=keywords)
             print(f"user said: {speech_as_text}")
 
             # Look for your "Ok Google" keyword in speech_as_text
-            if "google" in speech_as_text or "hey google":
-                greeting()
+            if "brybot" in speech_as_text or "hey brybot":
+                brybot.greeting()
                 recognize_main()
 
         except sr.UnknownValueError:
@@ -203,7 +166,7 @@ def command():
         try:
             query = r.recognize_google(audio_data)
             print(f"user said: {query}")
-            startBryBot(query)
+            brybot.startbryBot(query)
 
         except:
             print("Oops! Didn't catch that")
@@ -217,11 +180,13 @@ def command():
     start_recognizer()
 
 if __name__ == "__main__":
-    local_dir = os.path.dirname(__file__)
-    playsound(os.path.join(local_dir,"sounds\\pokemon-red_blue_yellow-item-found-sound-effect.mp3"))
-    #Use threading to execute both functions
-    x = threading.Thread(target=displayGIF, args=(local_dir,),daemon=True)
-    y = threading.Thread(target=command, args=())
-
-    x.start()
-    y.start()
+    # local_dir = os.path.dirname(__file__)
+    # playsound(os.path.join(local_dir,"sounds\\pokemon-red_blue_yellow-item-found-sound-effect.mp3"))
+    # #Use threading to execute both functions
+    # x = threading.Thread(target=displayGIF, args=(local_dir,),daemon=True)
+    # y = threading.Thread(target=command, args=())
+    #
+    # x.start()
+    # y.start()
+    brybot = Brybot()
+    command(brybot)
